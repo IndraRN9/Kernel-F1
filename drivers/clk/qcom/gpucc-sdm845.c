b723792d19dc0e31ec9cdd443ee93397af57a291
Ndra <80077633+IndraRN9@users.noreply.github.com>
2024-06-26 21:25:36 +0700

    Add files via upload

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -278,10 +278,10 @@ static const struct freq_tbl  ftbl_gpu_cc_gx_gfx3d_clk_src_sdm845_v2[] = {
 	F(257000000, P_CRC_DIV,  1, 0, 0),
 	F(342000000, P_CRC_DIV,  1, 0, 0),
 	F(414000000, P_CRC_DIV,  1, 0, 0),
-	F(540000000, P_CRC_DIV,  1, 0, 0),
-	F(730000000, P_CRC_DIV,  1, 0, 0),
-	F(800000000, P_CRC_DIV,  1, 0, 0),
-	F(825000000, P_CRC_DIV,  1, 0, 0),
+	F(520000000, P_CRC_DIV,  1, 0, 0),
+	F(720000000, P_CRC_DIV,  1, 0, 0),
+	F(810000000, P_CRC_DIV,  1, 0, 0),
+	F(860000000, P_CRC_DIV,  1, 0, 0),
 	{ }
 };
 
@@ -634,12 +634,12 @@ static void gpu_cc_gfx_sdm845_fixup_sdm845v2(void)
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_LOW_L1] =
 		414000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_NOMINAL] =
-		540000000;
+		520000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_NOMINAL_L1] =
-		730000000;
-	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 800000000;
+		720000000;
+	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 810000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L1] =
-		825000000;
+		860000000;
 }
 
 static void gpu_cc_gfx_sdm845_fixup_sdm670(void)
